fb747ba188ed6f318c56e7c06ab51f5c2503a2e9
bobbylightts <markdaoud23@gmail.com>
2014-04-22 14:35:31 -0700

    initial commit

---
+++ GPA Calculator/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  GPA Calculator
+//
+//  Created by Mark Daoud on 4/15/14.
+//  Copyright (c) 2014 Mark Daoud. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int total;
+    int earned;
+    double results;
+    
+    cout << "Enter the grades earned per class";
+    cin  >> earned;
+    
+    cout << "Enter the units per class";
+    cin  >> total;
+    
+    do{
+        results=(earned * total) / total;
+        cout << "Your GPA is " << results;
+        cout << " Do you want to calculate your GPA agian? yes (y) or no (n)";
+        cin  >> again;
+        while(agian == 'y' || agiain ++'Y');
+    }
+    
+}
+
+
